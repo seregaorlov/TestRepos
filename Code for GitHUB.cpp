2ea50555be06bcb9abda3513bf139c670c118b79
seregaorlov <166933218+seregaorlov@users.noreply.github.com>
2024-04-20 18:06:28 +0300

    First commit

---
+++ Code for GitHUB.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string name;
+    cout << "Введите ваше имя: ";
+    cin >> name;
+    cout << "Привет, " << name << "!" << endl;
+    return 0;
+}
